18a942107b3c88b9bf5b5dcf26bf2a8e1a23972b
ivan0961 <incivan_09@hotmail.com>
2016-08-29 01:05:40 +0300

    First commit

---
+++ ResimTahminOyunu/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace ResimTahminOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbBig = new System.Windows.Forms.PictureBox();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.rb1 = new System.Windows.Forms.RadioButton();
+            this.rb2 = new System.Windows.Forms.RadioButton();
+            this.rb3 = new System.Windows.Forms.RadioButton();
+            this.btnTamin = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBig)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbBig
+            // 
+            this.pbBig.Location = new System.Drawing.Point(5, 8);
+            this.pbBig.Name = "pbBig";
+            this.pbBig.Size = new System.Drawing.Size(571, 247);
+            this.pbBig.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbBig.TabIndex = 0;
+            this.pbBig.TabStop = false;
+            // 
+            // pb1
+            // 
+            this.pb1.Image = global::ResimTahminOyunu.Properties.Resources.resim1;
+            this.pb1.Location = new System.Drawing.Point(12, 291);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(150, 81);
+            this.pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb1.TabIndex = 1;
+            this.pb1.TabStop = false;
+            // 
+            // pb2
+            // 
+            this.pb2.Image = global::ResimTahminOyunu.Properties.Resources.resim2;
+            this.pb2.Location = new System.Drawing.Point(213, 291);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(150, 82);
+            this.pb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb2.TabIndex = 2;
+            this.pb2.TabStop = false;
+            // 
+            // pb3
+            // 
+            this.pb3.Image = global::ResimTahminOyunu.Properties.Resources.resim4;
+            this.pb3.Location = new System.Drawing.Point(418, 291);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(150, 81);
+            this.pb3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb3.TabIndex = 3;
+            this.pb3.TabStop = false;
+            // 
+            // rb1
+            // 
+            this.rb1.AutoSize = true;
+            this.rb1.Checked = true;
+            this.rb1.Location = new System.Drawing.Point(79, 378);
+            this.rb1.Name = "rb1";
+            this.rb1.Size = new System.Drawing.Size(14, 13);
+            this.rb1.TabIndex = 4;
+            this.rb1.TabStop = true;
+            this.rb1.UseVisualStyleBackColor = true;
+            this.rb1.CheckedChanged += new System.EventHandler(this.rb1_CheckedChanged);
+            // 
+            // rb2
+            // 
+            this.rb2.AutoSize = true;
+            this.rb2.Location = new System.Drawing.Point(280, 378);
+            this.rb2.Name = "rb2";
+            this.rb2.Size = new System.Drawing.Size(14, 13);
+            this.rb2.TabIndex = 5;
+            this.rb2.UseVisualStyleBackColor = true;
+            this.rb2.CheckedChanged += new System.EventHandler(this.rb2_CheckedChanged);
+            // 
+            // rb3
+            // 
+            this.rb3.AutoSize = true;
+            this.rb3.Location = new System.Drawing.Point(496, 378);
+            this.rb3.Name = "rb3";
+            this.rb3.Size = new System.Drawing.Size(14, 13);
+            this.rb3.TabIndex = 6;
+            this.rb3.UseVisualStyleBackColor = true;
+            this.rb3.CheckedChanged += new System.EventHandler(this.rb3_CheckedChanged);
+            // 
+            // btnTamin
+            // 
+            this.btnTamin.Enabled = false;
+            this.btnTamin.Location = new System.Drawing.Point(252, 261);
+            this.btnTamin.Name = "btnTamin";
+            this.btnTamin.Size = new System.Drawing.Size(75, 23);
+            this.btnTamin.TabIndex = 7;
+            this.btnTamin.Text = "TAHMİN";
+            this.btnTamin.UseVisualStyleBackColor = true;
+            this.btnTamin.Click += new System.EventHandler(this.btnTamin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 413);
+            this.Controls.Add(this.btnTamin);
+            this.Controls.Add(this.rb3);
+            this.Controls.Add(this.rb2);
+            this.Controls.Add(this.rb1);
+            this.Controls.Add(this.pb3);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.pbBig);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbBig)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbBig;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.RadioButton rb1;
+        private System.Windows.Forms.RadioButton rb2;
+        private System.Windows.Forms.RadioButton rb3;
+        private System.Windows.Forms.Button btnTamin;
+    }
+}
+
---
+++ ResimTahminOyunu/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ResimTahminOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int tahmin = 0, dogruTahmin = 0;
+
+        private void btnTamin_Click(object sender, EventArgs e)
+        {
+            tahmin++;//her denemede tahmin sayısını arttır. (ister doğru tahmin olsun ister yanlış tahmin)
+
+            Random gen = new Random();
+            int sayi = gen.Next(1, 4);
+            switch (sayi)
+            {
+                case 1:
+                    pbBig.Image = Properties.Resources.resim1;
+                    if (rb1.Checked)
+                    {
+                        dogruTahmin++;
+                        Tebrik();
+                    }
+                    break;
+                case 2:
+                    pbBig.Image = Properties.Resources.resim2;
+                    if (rb2.Checked)
+                    {
+                        dogruTahmin++;
+                        Tebrik();
+                    }
+                    break;
+                case 3:
+                    pbBig.Image = Properties.Resources.resim4;
+                    if (rb3.Checked)
+                    {
+                        dogruTahmin++;
+                        Tebrik();
+                    }
+                    break;
+
+            }
+
+           
+        }
+
+        private void Tebrik()
+        {
+            MessageBox.Show(string.Format("Tebrikler!..\n{0} denemede {1}. doğru tahmininiz.", tahmin, dogruTahmin), "Vuu huuuu");
+            pbBig.Image = null;//pbBig içeriğini temizle.
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rb1.Checked = false;
+        }
+        //butonun aktif olabilmesi herhangi bir radio butonun seçilmesine bağlı.
+
+        void BtnAktif(RadioButton radio)
+        {
+            if (radio.Checked == true)
+            {
+                btnTamin.Enabled = true;
+            }
+        }
+
+
+        private void rb1_CheckedChanged(object sender, EventArgs e)
+        {
+            BtnAktif(rb1);
+        }
+
+        private void rb2_CheckedChanged(object sender, EventArgs e)
+        {
+            BtnAktif(rb2);
+        }
+
+        private void rb3_CheckedChanged(object sender, EventArgs e)
+        {
+            BtnAktif(rb3);
+        }
+    }
+}
